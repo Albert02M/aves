21c5137f574c0e4dbf5876ddb217981c8ba80abe
Albert02M <albertomoralesdev@gmail.com>
2024-12-03 23:16:49 -0600

    se añade rollbar

--- config/logging.php
+++ config/logging.php
@@ -49,11 +49,17 @@ return [
     |                    "custom", "stack"
     |
     */
-
     'channels' => [
-        'stack' => [
+
+        'rollbar' => [
+            'driver' => 'monolog',
+            'handler' => \Rollbar\Laravel\MonologHandler::class,
+            'access_token' => env('ROLLBAR_TOKEN'),
+            'level' => 'debug',
+        ],
+'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['rollbar','single'],
             'ignore_exceptions' => false,
         ],
 
@@ -117,6 +123,8 @@ return [
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
         ],
+
     ],
+
 
 ];
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/test-rollbar', function () {
+//     throw new Exception("¡Esto es una prueba de Rollbar!");
+// });
